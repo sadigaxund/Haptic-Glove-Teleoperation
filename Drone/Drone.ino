3527428155615c73cf1bafddb52e458eb1f3d0e5
Sadig <sadigaxund@gmail.com>
2021-03-24 02:14:33 +0400

    24:03:21 | + Added: Basic movement, developer button with interrupt pin 2, some auxilary setters for easier control

--- Drone/Drone.ino
+++ Drone/Drone.ino
@@ -13,11 +13,39 @@
 #define wheelInL2       38  // Control pin 2 for the left motor
 #define wheelInR1       40  // Control pin 1 for the right motor
 #define wheelInR2       42  // Control pin 2 for the right motor
+#define interruptPin    2  // 
 
 RH_ASK driver;
+int delayTime = 50; // ms
+
+enum CMD{STOP=0, FORWARD=1, BACKWARD=2, LEFT=3, RIGHT=4};
+enum CMD droneControl = STOP;
+
+
+void blink() {
+  next();
+}
+
+void next()
+{
+    if(droneControl == STOP)
+    droneControl = FORWARD;
+  else if(droneControl == FORWARD)
+    droneControl = BACKWARD;
+  else if(droneControl == BACKWARD)
+    droneControl = LEFT;
+  else if(droneControl == LEFT)
+    droneControl = RIGHT;
+ else if(droneControl == RIGHT)
+    droneControl = STOP;
+}
+
 
 void setup()
 {
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+  
   /*****Define mode for each pin*****/
 
   /*INPUTS*/
@@ -38,14 +66,64 @@ void setup()
   Serial.begin(9600);    // Debugging only
   if (!driver.init())
     Serial.println("init failed");
+
+  fwd();
+  delay(3000);
+  fwd_right();
+  delay(1000);
+  fwd();
+  delay(3000);
+  fwd_right();
+  delay(1000);
+  fwd();
+  delay(3000);
+  fwd_right();
+  delay(1000);
+  fwd();
+  delay(3000);
+  fwd_right();
+  delay(1000);
+
 }
 
-
+  //L ->  LOW, HIGH = FORWARD
+  //R ->  HIGH, LOW = FORWARD
 void loop()
 {
-
+  delay(delayTime);
+ 
+ 
+  while(true)
+  {
+    switch(droneControl)
+    {
+      case STOP:
+      setRightMotor(LOW, LOW);
+      setLeftMotor(LOW, LOW);
+      break;
+      case FORWARD:
+      setRightMotor(LOW, HIGH);
+      setLeftMotor(HIGH, LOW);
+      break;
+      case BACKWARD:
+      setRightMotor(HIGH, LOW);
+      setLeftMotor(LOW, HIGH);
+      break;
+      case LEFT:
+      setRightMotor(LOW, LOW);
+      setLeftMotor(HIGH, LOW);
+      break;
+      case RIGHT:
+      setRightMotor(LOW, HIGH);
+      setLeftMotor(LOW, LOW);
+      break;
+      
+    }
+  }
   
 }
+
+
 
 void transmit()
 {
@@ -90,34 +168,65 @@ void setSpeed(int speed)
   //analogWrite(ctrlSpeedPin, speed);
 }
 
+
+
 /** Function used for setting the output pins from motor controller IC. Controls Right motor*/
 void setLeftMotor(int pinOut1, int pinOut2)
 {
+  //LOW, HIGH = FORWARD
   digitalWrite(wheelInL1, pinOut1);
   digitalWrite(wheelInL2, pinOut2);
 }
+  
 /** Function used for setting the output pins from motor controller IC. Controls Left motor*/
 void setRightMotor(int pinOut1, int pinOut2)
 {
-  digitalWrite(wheelInL1, pinOut1);
-  digitalWrite(wheelInL2, pinOut2);
+  //HIGH, LOW = FORWARD
+  digitalWrite(wheelInR1, pinOut1);
+  digitalWrite(wheelInR2, pinOut2);
 }
 
 
 /** Function used to stop the motor by setting the input voltages of motor to LOW*/
 void stopMotor()
 {
- 
+  setRightMotor(LOW, LOW);
+  setLeftMotor(LOW, LOW);
 }
 
 /** Function used to make motor move forwards*/
-void forwards()
+void fwd()
 {
-
+  setRightMotor(HIGH, LOW);
+  setLeftMotor(LOW, HIGH);
 }
 
 /** Function used to make motor move backwards*/
-void backwards()
+void bkw()
 {
+  setRightMotor(LOW, HIGH);
+  setLeftMotor(HIGH, LOW);
+}
 
+void fwd_right()
+{
+  fwd();
+  setLeftMotor(LOW, LOW);
+}
+void fwd_left()
+{
+  fwd();
+  setRightMotor(LOW, LOW);
+}
+
+void bkw_right()
+{
+  bkw();
+  setLeftMotor(LOW, LOW);
+}
+
+void bkw_left()
+{
+  bkw();
+  setRightMotor(LOW, LOW);
 }
